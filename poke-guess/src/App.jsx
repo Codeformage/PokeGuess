066da09cd35657f3ebcc61e866b0eebf1220b70a
Codeformage <194207968+Codeformage@users.noreply.github.com>
2025-02-26 05:40:04 -0500

    Components skeleton created: Landing Container Life POkeball instruction Congrats

--- poke-guess/src/App.jsx
+++ poke-guess/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import {Container} from './components/Container';
 
 function App() {
 
   return (
     <>
       <h1>Vite + React</h1>
+      <Container />
     </>
   )
 }
---
+++ poke-guess/src/components/Container.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export function Container () {
+    return (
+        <div>Container element</div>
+    )
+}
